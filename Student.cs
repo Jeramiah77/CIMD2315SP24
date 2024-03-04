e658f8405222a5e5c4e1bafeef636f5519808465
Jeramiah77 <156719275+Jeramiah77@users.noreply.github.com>
2024-03-04 13:06:32 -0600

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,14 @@
+
+
+class Student{
+
+    public string Name;
+    public string enrolled;
+    public double grade;
+    public void PrintInfo(){
+        Console.WriteLine($" Student {Name}, enrolls in {enrolled} and the grade is  {grade} ");
+    }
+}
+
+
+
